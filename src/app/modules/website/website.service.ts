1a181dddeddf52f9824d69abe480ff37949ca13d
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-01-17 14:29:13 +0600

    fix manage website

--- src/app/modules/website/website.service.ts
+++ src/app/modules/website/website.service.ts
@@ -1,31 +1,46 @@
 import AppError from '../../error/appError';
+import { fileUploader } from '../../helper/fileUploder';
 import pagination, { IOption } from '../../helper/pagenation';
 import { userRole } from '../user/user.constant';
 import User from '../user/user.model';
-import { IWebsite } from './website.interface';
+import { IManageWebsite } from './website.interface';
+// import { IWebsite } from './website.interface';
 import Website from './website.model';
 
-const createWebsite = async (userId: string, payload: Partial<IWebsite>) => {
-  const user = await User.findById(userId);
-  if (!user) throw new AppError(404, 'User not found');
-  const result = Website.create({ ...payload, createBy: user.id });
+const createWebsite = async (
+  payload: Partial<IManageWebsite>,
+  files?: Express.Multer.File[],
+) => {
+  if (files && files.length > 0) {
+    const images = await Promise.all(
+      files.map(async (file) => {
+        const result = await fileUploader.uploadToCloudinary(file);
+        return result.url;
+      }),
+    );
+    payload.thumbnail = images;
+  }
+  const result = Website.create(payload);
   if (!result) throw new AppError(400, 'Website not created');
   return result;
 };
 
 const updateWebsite = async (
-  userId: string,
   id: string,
-  payload: Partial<IWebsite>,
+  payload: Partial<IManageWebsite>,
+  files?: Express.Multer.File[],
 ) => {
-  const user = await User.findById(userId);
-  if (!user) throw new AppError(404, 'User not found');
-
   const isExist = await Website.findById(id);
   if (!isExist) throw new AppError(404, 'Website not found');
-  if (user.role !== userRole.admin) {
-    if (isExist.createBy.toString() !== userId)
-      throw new AppError(403, 'You are not authorized to update this website');
+
+  if (files && files.length > 0) {
+    const images = await Promise.all(
+      files.map(async (file) => {
+        const result = await fileUploader.uploadToCloudinary(file);
+        return result.url;
+      }),
+    );
+    payload.thumbnail = images;
   }
   const result = await Website.findByIdAndUpdate(id, payload, {
     new: true,
@@ -35,12 +50,34 @@ const updateWebsite = async (
   return result;
 };
 
+// const updateWebsite = async (
+//   userId: string,
+//   id: string,
+//   payload: Partial<IManageWebsite>,
+// ) => {
+//   const user = await User.findById(userId);
+//   if (!user) throw new AppError(404, 'User not found');
+
+//   const isExist = await Website.findById(id);
+//   if (!isExist) throw new AppError(404, 'Website not found');
+//   if (user.role !== userRole.admin) {
+//     if (isExist.createBy.toString() !== userId)
+//       throw new AppError(403, 'You are not authorized to update this website');
+//   }
+//   const result = await Website.findByIdAndUpdate(id, payload, {
+//     new: true,
+//     runValidators: true,
+//   });
+//   if (!result) throw new AppError(400, 'Website not updated');
+//   return result;
+// };
+
 const getAllWebsite = async (params: any, options: IOption) => {
   const { page, limit, skip, sortBy, sortOrder } = pagination(options);
   const { searchTerm, year, ...filterData } = params;
 
   const andCondition: any[] = [];
-  const userSearchableFields = ['key', 'type', 'title'];
+  const userSearchableFields = ['subtitle', 'type', 'title', 'description'];
 
   if (searchTerm) {
     andCondition.push({
@@ -100,22 +137,29 @@ const getWebsiteById = async (id: string) => {
   return result;
 };
 
-const getWebsiteByKey = async (key: string) => {
-  const result = await Website.findOne({ key });
+const deleteWebsite = async (id: string) => {
+  const result = await Website.findByIdAndDelete(id);
   if (!result) throw new AppError(404, 'Website not found');
   return result;
 };
 
-const removeWebsite = async (id: string) => {
-  const result = await Website.findByIdAndDelete(id);
-  return result;
-};
+// const getWebsiteByKey = async (key: string) => {
+//   const result = await Website.findOne({ key });
+//   if (!result) throw new AppError(404, 'Website not found');
+//   return result;
+// };
+
+// const removeWebsite = async (id: string) => {
+//   const result = await Website.findByIdAndDelete(id);
+//   return result;
+// };
 
 export const websiteService = {
   createWebsite,
   updateWebsite,
   getAllWebsite,
   getWebsiteById,
-  getWebsiteByKey,
-  removeWebsite,
+  deleteWebsite,
+  // getWebsiteByKey,
+  // removeWebsite,
 };
