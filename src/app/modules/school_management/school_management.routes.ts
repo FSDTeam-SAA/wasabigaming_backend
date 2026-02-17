f8340b27cca1c1ec8c44035a3bfd618677d778d2
Wrong1234 <mahabur24cse@gmail.com>
2026-02-17 12:39:18 +0600

    fix subscription issue in school

--- src/app/modules/school_management/school_management.routes.ts
+++ src/app/modules/school_management/school_management.routes.ts
@@ -2,11 +2,12 @@ import express from 'express'
 import { userRole } from '../user/user.constant';
 import auth from '../../middlewares/auth';
 import { schoolManagementController } from './school_management.controller';
+import { checkStudentSubscription } from '../../middlewares/checkSubscription';
 
 const router = express.Router()
 
-router.get('/overview', auth(userRole.school), schoolManagementController.schoolOverview)
-router.get('/', auth(userRole.school), schoolManagementController.getAllStudents)
+router.get('/overview', auth(userRole.school), checkStudentSubscription, schoolManagementController.schoolOverview)
+router.get('/', auth(userRole.school), checkStudentSubscription, schoolManagementController.getAllStudents)
 router.get('/:id', auth(userRole.school), schoolManagementController.getSingleStudent)
 router.delete('/:id', auth(userRole.school), schoolManagementController.deleteStudent)
 
