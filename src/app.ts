fc6740d950c5f570a8c185edb4ba032454cc3426
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-02-21 09:23:46 +0600

    update cors configuration to include additional allowed origins

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,9 @@ app.use(cors({
     'http://127.0.0.1:5500', 
     'http://localhost:3000',
     'http://localhost:3001',
-    'http://localhost:5173',  // add if using Vite
+    'http://localhost:5173', 
+    'https://aspiringlegalnetwork.co.uk',
+    'https://admin.aspiringlegalnetwork.co.uk' 
   ], 
   credentials: true 
 }));
